2514fb37fa9ceea9f17f6349e9171cf650b98e05
topdeveloper0729 <topdev0729@gmail.com>
2021-05-07 08:21:48 +0900

    feat: add Counter and Table page and add react-bootstrap npm

---
+++ src/pages/Counter/index.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+
+const Counter = () => {
+	const [counter, setCounter] = useState('');
+
+	useEffect(() => {
+
+	});
+
+	return(
+		<>
+
+		</>
+	)
+}
+
+export default Counter;
---
+++ src/pages/Table/index.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+
+const Table = () => {
+	const [state, setState] = useState('');
+
+	useEffect(() => {
+
+	});
+
+	return (
+		<>
+				
+		</>
+	)
+}
+
+export default Table;
